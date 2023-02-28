641179e9252686a1363cc6c19c4fc1bfb9309653
Marcus Vinícius Bizerra Moreira <100208378+MarcusVBMoreira@users.noreply.github.com>
2023-02-28 11:03:59 -0300

    Add files via upload

---
+++ assets/java.js
@@ -0,0 +1,11 @@
+function abrirpagina(link){
+    switch(link){
+        case 1: window.location.href = "pagina2.html"
+        break
+        case 2: window.location.href = "pagina2.html"
+        break
+        case 3: window.location.href = "pagina2.html"
+        break
+    }
+    
+}
